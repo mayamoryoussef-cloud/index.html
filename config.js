341464c02a11bb49ae3470a9cf47049408b91a21
mayamoryoussef-cloud <maya.moryoussef@gmail.com>
2026-02-08 01:27:59 -0500

    Update config.js

--- config.js
+++ config.js
@@ -63,7 +63,7 @@ secretAnswer: "No im ur wife 😁" // Hidden message
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/ddc5rfylc/video/upload/v1770525889/The_Cranberries_-_Linger_Official_Music_Video_4_s8aae0.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
